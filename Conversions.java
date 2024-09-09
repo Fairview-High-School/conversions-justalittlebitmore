65e12d355d2c03c0ab297de094f315710ff604b0
justalittlebitmore <asjames01@bvsd.org>
2024-09-09 14:46:34 -0600

    Update Conversions.java

--- Conversions.java
+++ Conversions.java
@@ -17,7 +17,11 @@ public class Conversions
         System.out.println("4. Meters to Feet");
         System.out.println("5. Ounces to Milliliters");
         System.out.println("6. Milliliters to Ounces");
-        //Add 2 more conversions, each both ways, to add choices 7, 8, 9, and 10.
+        System.out.println("7. pounds to ');
+        System.out.println("8.');
+        System.out.println("9.');
+        System.out.println("10.');
+       //Add 2 more conversions, each both ways, to add choices 7, 8, 9, and 10.
         //You can add even more if you would like.
 
         int selection = keyboard.nextInt();
@@ -33,6 +37,11 @@ public class Conversions
         }
         if (selection == 2)
         {
+            System.out.println("Enter fahrenheit: ");
+            double fahrenheit = keyboard.nextDouble();
+            keyboard.nextLine();
+            double fahrenheit = celsius * (9.0 / 5.0) + 32;
+            System.out.println(celsius + " fahrenheit is " + degrees celsius + " degrees celsius");
             //add the code to ask the user to enter Fahrenheit and then convert to Celsius
         }
         //add more if statements here.
